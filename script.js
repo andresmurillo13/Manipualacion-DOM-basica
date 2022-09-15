1ac751c0b5e9c5f3bee23633c0c27dcf8566dffe
Andres Murillo <andresfelipemurillobonilla@gmail.com>
2022-09-15 01:13:00 -0500

    Manipulacion de dom basico

---
+++ script.js
@@ -0,0 +1,13 @@
+const h1 = document.querySelector('h1');
+const parrafo1 = document.querySelector('.parrafo1');
+const parrafo2 = document.querySelector('#parrafo2');;
+const input = document.querySelector('input')
+
+console.log(input.value);
+
+console.log({
+    h1,
+    parrafo1,
+    parrafo2,
+    input
+});
